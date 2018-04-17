9ca2d9770d09da9686ca5a9b322dd2c02aeb1305
Sarah Laine <sjlaine624@gmail.com>
2018-04-17 11:27:17 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
